d71bdf89409fd04f3a47760f9d905b7e3a8267d3
Nevojt <nevojt@gmail.com>
2025-03-13 20:06:52 +0100

    Revert "test setting email"
    This reverts commit 9b945ef993c5409850e043a7307b7a840a7f905d.

--- backend/internal/adminpanel/services/utils/email.go
+++ backend/internal/adminpanel/services/utils/email.go
@@ -1,125 +1,48 @@
 package utils
 
 import (
-	"context"
-
-	"encoding/base64"
-	"encoding/json"
+	"backend/internal/adminpanel/entities"
+	"crypto/tls"
 	"fmt"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/gmail/v1"
-	"google.golang.org/api/option"
-
+	"github.com/joho/godotenv"
+	"gopkg.in/gomail.v2"
 	"log"
-	"net/http"
 	"os"
 )
 
-// Retrieve a token, saves the token, then returns the generated client.
-func getClient(config *oauth2.Config) *http.Client {
-	// The file token.json stores the user's access and refresh tokens, and is
-	// created automatically when the authorization flow completes for the first
-	// time.
-	tokFile := "token.json"
-	tok, err := tokenFromFile(tokFile)
+func SendEmail(to string, subject string, body string, isHTML bool) error {
+	err := godotenv.Load(".env")
 	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
+		return fmt.Errorf("error loading .env file email: %v", err)
 	}
-	return config.Client(context.Background(), tok)
-}
-
-// Request a token from the web, then returns the retrieved token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
-
-	var authCode string
-	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code: %v", err)
+	var emailConfig = entities.EmailConfig{
+		SMTPHost: os.Getenv("SMTP_HOST"),
+		SMTPPort: 587,
+		Username: os.Getenv("SMTP_USER"),
+		Password: os.Getenv("SMTP_PASSWORD"),
+		From:     os.Getenv("EMAILS_FROM_EMAIL"),
 	}
 
-	tok, err := config.Exchange(context.TODO(), authCode)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web: %v", err)
-	}
-	return tok
-}
+	mailer := gomail.NewMessage()
+	mailer.SetHeader("From", emailConfig.From)
+	mailer.SetHeader("To", to)
+	mailer.SetHeader("Subject", subject)
 
-// Retrieves a token from a local file.
-func tokenFromFile(file string) (*oauth2.Token, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	defer func(f *os.File) {
-		err := f.Close()
-		if err != nil {
-
-		}
-	}(f)
-	tok := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(tok)
-	return tok, err
-}
-
-// Saves a token to a file path.
-func saveToken(path string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", path)
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	defer func(f *os.File) {
-		err := f.Close()
-		if err != nil {
-
-		}
-	}(f)
-	json.NewEncoder(f).Encode(token)
-}
-
-//"backend/credentials_google_gmail.json"
-
-// Функція для відправки email
-func SendEmail(to, subject, body string) error {
-	ctx := context.Background()
-	b, err := os.ReadFile("credentials_google_gmail.json")
-	if err != nil {
-		return fmt.Errorf("unable to read client secret file: %v", err)
+	if isHTML {
+		mailer.SetBody("text/html", body)
+	} else {
+		mailer.SetBody("text/plain", body)
 	}
 
-	config, err := google.ConfigFromJSON(b, gmail.GmailSendScope)
-	if err != nil {
-		return fmt.Errorf("unable to parse client secret file to config: %v", err)
+	dialer := gomail.NewDialer(emailConfig.SMTPHost, emailConfig.SMTPPort, emailConfig.Username, emailConfig.Password)
+	dialer.TLSConfig = &tls.Config{ServerName: emailConfig.SMTPHost}
+
+	if err := dialer.DialAndSend(mailer); err != nil {
+		log.Printf("Failed to send email to %s: %v", to, err)
+		return err
 	}
 
-	client := getClient(config)
-	srv, err := gmail.NewService(ctx, option.WithHTTPClient(client))
-	if err != nil {
-		return fmt.Errorf("unable to retrieve Gmail client: %v", err)
-	}
-
-	from := os.Getenv("EMAILS_FROM_EMAIL")
-	if from == "" {
-		return fmt.Errorf("EMAILS_FROM_EMAIL is not set")
-	}
-
-	// Формування повідомлення у форматі RFC 2822
-	message := fmt.Sprintf("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n%s", from, to, subject, body)
-	encodedMessage := base64.URLEncoding.EncodeToString([]byte(message))
-
-	email := &gmail.Message{Raw: encodedMessage}
-
-	_, err = srv.Users.Messages.Send("me", email).Do()
-	if err != nil {
-		return fmt.Errorf("u"+
-			"nable to send email: %v", err)
-	}
-
-	log.Println("Email successfully sent to", to)
+	log.Printf("Email successfully sent to %s", to)
 	return nil
 }
 
@@ -134,5 +57,5 @@ func SendPasswordResetEmail(to string, resetToken string) error {
 	`, resetLink)
 
 	subject := "Password Reset Request"
-	return SendEmail(to, subject, htmlBody)
+	return SendEmail(to, subject, htmlBody, true)
 }
