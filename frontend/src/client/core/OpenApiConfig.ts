2a4c5276cdf29a89137e45c5f6c0177134d8e8e2
Nevojt <nevojt@gmail.com>
2025-03-25 21:30:32 +0100

    update openAPI 2

--- frontend/src/client/core/OpenApiConfig.ts
+++ frontend/src/client/core/OpenApiConfig.ts
@@ -1,19 +1,11 @@
 import { OpenAPI } from "./OpenAPI"
 
+import { getApiUrl } from "../../utils/urls"
+
 export const updateOpenApiConfig = () => {
     const token = localStorage.getItem("access_token") || ""
-    const domain = localStorage.getItem("tenant") || "localhost"
-
-    const isDev = import.meta.env.DEV
-    const port = isDev ? ":5180" : ""
-    const protocol = isDev ? "http" : "https"
-
-    const baseUrl = isDev
-        ? `${protocol}://${domain}.localhost${port}`
-        : `${protocol}://${domain}.dbgone.com`
-
+    OpenAPI.BASE = getApiUrl()
     OpenAPI.TOKEN = async () => token
-    OpenAPI.BASE = baseUrl
 }
 
 
